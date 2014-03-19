64d1b870c85b58fb2501a295e14580fa0d846eae
Surbhi <surbhishah81@yahoo.in>
2014-03-19 07:53:31 +0530

    Edited PCA2final, pca complete

--- pca2final.py
+++ pca2final.py
@@ -23,15 +23,11 @@ data = np.delete(data, n-1,0);
 
 # Transform the data to get zero mean 
 meanMatrix = np.mean(data,axis=1);
-print meanMatrix;
+#print meanMatrix;
 n,m = data.shape;
 for i in range(n):
 	for j in range(m):
 		data[i][j] -= meanMatrix[i];
-
-
-# Transform by dividing by standard deviation
-data /= data.std(axis=0);
 		
 		
 # Get the transpose of data 
@@ -39,26 +35,32 @@ transpose = np.transpose(data);
 
 
 # Get dot product of data and its transpose to get covariance matrix times n-1
-dotProduct = np.dot(data,transpose);
+covMatrix = np.cov(data);
+print "covMatrix";
+print covMatrix;
 
-
-# Get Eigen Values and Eigen vectors
-eigenValues,eigenVectors = LA.eigh(dotProduct);
+#Get eigen values and eigen vectors 
+eigenValues,eigenVectors = LA.eigh(covMatrix);
+print "eigenVectors";
+print eigenVectors;
+print "eigenvalues"
+print eigenValues;
 
 
 # Sort Eigen values in decreasing order
 idx = eigenValues.argsort()[::-1]   
 eigenValues = eigenValues[idx]
 eigenVectors = eigenVectors[:,idx]
-print "Eigen values:"
+print "Eigen values: Sorted"
 print eigenValues;
-print "Eigen vectors:"
+print "Eigen vectors: Sorted"
 print eigenVectors;
 
 
-# Matrix of principal components is transpose of matrix of eigen vectors
+#Compute the transformed matrix 
+# Matrix of principal components is transpose of matrix of eigen vectors ie (eig1 eig2 eig3)
 P = np.transpose(eigenVectors);
-print "P";
+print "Principal Components";
 print P;
 
 # Transform Y = PX to get new data
@@ -67,8 +69,6 @@ print "Y";
 
 # Take transpose for convenience in viewing data
 y = np.transpose(y);
-print y;
-
-
-
+np.savetxt("output.npy",y);
+#print y;
 
